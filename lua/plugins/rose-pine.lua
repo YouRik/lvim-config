d3689a11dbb6e44202c983d0bcfdc6aa8ff155f4
Jurik Karimian <yourik@live.de>
2025-09-23 11:32:33 +0200

    Fix colorscheme setup/config and enable transparency

--- lua/plugins/rose-pine.lua
+++ lua/plugins/rose-pine.lua
@@ -1,15 +1,17 @@
 return {
   "rose-pine/neovim",
+  name = "rose-pine",
   opts = {
     -- variant = "auto",
     -- dark_variant = "main",
     dim_inactive_windows = false,
     extend_background_behind_borders = true,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "rose-pine",
+    styles = {
+      transparency = true,
     },
   },
+  config = function(_, opts)
+    require("rose-pine").setup(opts)
+    vim.cmd("colorscheme rose-pine")
+  end,
 }
